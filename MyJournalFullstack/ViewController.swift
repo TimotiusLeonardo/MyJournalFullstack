ee9788d4e1e87970ecb1c45373a794c6b6790565
Timotius Leonardo Lianoto <lionato14@gmail.com>
2021-05-06 11:00:38 +0700

    Initial Commit

---
+++ MyJournalFullstack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyJournalFullstack
+//
+//  Created by IDN MEDIA on 06/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
